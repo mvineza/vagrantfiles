e6978bd1d5609ac42b2cfae6a13b08d6e3f52014
Merrell Vineza <rell.vineza14@yahoo.com>
2019-11-11 18:56:38 +0800

    consolidate-environments: cleanup script

--- create_environment.py
+++ create_environment.py
@@ -2,7 +2,7 @@
 
 import os
 import re
-from jinja2 import Template, Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader
 from argparse import ArgumentParser
 
 work_dir = 'workdir'
@@ -12,7 +12,7 @@ dump_file = 'Vagrantfile'
 subnet = '192.168.50'
 host_file = '/etc/hosts'
 
-parser = ArgumentParser(description='Setups environment using Vagrant and Ansible')
+parser = ArgumentParser(description='Setups environment using Vagrant/Ansible')
 parser.add_argument('-c', dest='count', default='1', help='number of VMs')
 args = parser.parse_args()
 
@@ -29,8 +29,11 @@ def get_last_ip():
     with open(host_file) as f:
         for line in f.readlines():
             ip_list.extend(re.findall(subnet + '.[0-9]+', line.strip()))
-    last_ip = sorted(ip_list)[-1:][0]
-    last_host = last_ip.split('.')[-1:][0]
+    try:
+        last_ip = sorted(ip_list)[-1:][0]
+        last_host = last_ip.split('.')[-1:][0]
+    except IndexError:
+        last_host = '1'
     return last_host
 
 
@@ -38,7 +41,13 @@ def render_template():
     last_host = get_last_ip()
     e = Environment(loader=FileSystemLoader(template_dir))
     t = e.get_template(template_file)
-    t.stream(count=count, subnet=subnet, last_host=last_host).dump(os.path.join(work_dir, dump_file))
+    t.stream(count=count, subnet=subnet, last_host=last_host).dump(
+        os.path.join(work_dir, dump_file))
+
+
+def bootstrap_environment():
+    pass
 
 
 render_template()
+bootstrap_environment()
